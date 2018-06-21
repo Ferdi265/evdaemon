8294617b2df2401d373b9e2f95f648f1cb47bfc7
Ferdinand Bachmann <theferdi265@gmail.com>
2018-06-21 10:46:43 +0200

    evdmodule_i3: wip i3 module

--- evdmodule_i3/module.py
+++ evdmodule_i3/module.py
@@ -3,11 +3,15 @@ from evdmodule_wm import *
 from .ipc import i3ipcModule
 
 class i3Module(wmModule):
+    """
+    An evd window manager module for the i3 window manager
+    """
     def __init__(self, ipc = None):
         super().__init__()
         self._ipc = ipc or i3ipcModule()
-       
-        self.listen("i3ipc", "reply", "tree", self._tree)
+
+        self.listen_once("i3ipc", "reply", "outputs", self._outputs)
+        self._ipc.send_cmd("outputs")
 
     def register_daemon(self, daemon):
         super().register_daemon(daemon)
@@ -17,5 +21,16 @@ class i3Module(wmModule):
         super().unregister_daemon()
         daemon.unregister(self._ipc)
     
+    def _outputs(self, payload):
+        monitors = []
+        for output in outputs:
+            o_rect = output['rect']
+            rect = Rect(o_rect['x'], o_rect['y'], o_rect['width'], o_rect['height'])
+            name = output['name']
+            monitor = Monitor(rect, name)
+            monitor.active = output['active']
+            monitor.primary = output['primary']
+
     def _tree(self, payload):
         print(payload)
+        print(payload.keys())
